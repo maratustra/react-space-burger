ee522a168841cc11e2cb307d93d95f6126a57601
Alena Maramygina <57060511+maratustra@users.noreply.github.com>
2024-08-18 20:04:26 +0200

    готова лента заказов

--- src/components/order-block/order-block.tsx
+++ src/components/order-block/order-block.tsx
@@ -5,40 +5,65 @@ import {
   FormattedDate,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
-const OrderBlock: React.FC = () => {
-  const today = new Date();
+interface Ingredient {
+  name: string;
+  image: string;
+}
+
+interface OrderBlockProps {
+  orderNumber: string;
+  completionDate: string;
+  burgerName: string;
+  burgerPrice: number;
+  ingredients: Ingredient[];
+}
+
+const OrderBlock: React.FC<OrderBlockProps> = ({
+  orderNumber,
+  completionDate,
+  burgerName,
+  burgerPrice,
+  ingredients,
+}) => {
+  const maxVisibleIngredients = 4;
+  const remainingCount = ingredients.length - maxVisibleIngredients;
 
   return (
     <div className={styles.orderBlock}>
       <div className={styles.orderInfo}>
-        <p className="text text_type_digits-default">#034533</p>
+        <p className="text text_type_digits-default">{orderNumber}</p>
         <FormattedDate
-          date={
-            new Date(
-              today.getFullYear(),
-              today.getMonth(),
-              today.getDate(),
-              today.getHours(),
-              today.getMinutes() - 1,
-              0
-            )
-          }
+          date={new Date(completionDate)}
           className={styles.timeStamp}
         />
       </div>
-      <p className="text text_type_main-medium">
-        Death Star Starship Main бургер
-      </p>
+      <p className="text text_type_main-medium">{burgerName}</p>
       <div className={styles.ingredientsSection}>
         <div className={styles.ingredients}>
-          <div className={styles.ingredient}>{/* First icon */}</div>
-          <div className={styles.ingredient}>{/* Second icon */}</div>
-          <div className={styles.ingredient}>{/* Third icon */}</div>
-          <div className={styles.ingredient}>{/* Fourth icon */}</div>
-          <div className={styles.ingredient}>{/* Fifth icon */}</div>
+          {ingredients.slice(0, maxVisibleIngredients).map((ingredient, index) => (
+            <div
+              key={index}
+              className={styles.ingredient}
+              style={{
+                left: `${index * 55}px`,
+                zIndex: maxVisibleIngredients - index,
+              }}
+            >
+              <img src={ingredient.image} alt={ingredient.name} />
+              {index === maxVisibleIngredients - 1 && remainingCount > 0 && (
+                <div className={styles.remainingCount}>
+                  <span>+{remainingCount}</span>
+                </div>
+              )}
+            </div>
+          ))}
         </div>
         <div className={styles["total-price"]}>
-          <Counter count={370} size="default" extraClass={styles.counter} />
+          <Counter
+            count={burgerPrice}
+            size="default"
+            extraClass={styles.counter}
+          />
           <CurrencyIcon type="primary" />
         </div>
       </div>
