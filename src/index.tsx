ca01076066a1d59bc23017f4e9874508440f6b9e
Alena Maramygina <57060511+maratustra@users.noreply.github.com>
2024-09-04 21:32:18 +0200

    подготовка к деплою на GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/app/app";
 import { store } from "./services/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
